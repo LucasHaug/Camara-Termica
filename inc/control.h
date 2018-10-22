423bec335f7d68651a34f2b7794a614bbba322b2
LucasHaug <39196309+LucasHaug@users.noreply.github.com>
2018-10-21 22:34:35 -0300

    Rename functions, reorganize sensors code and add control code

--- inc/control.h
+++ inc/control.h
@@ -1,4 +1,6 @@
 #ifndef __CONTROL_H__
 #define __CONTROL_H__
 
+void pi_action(void);
+
 #endif // __CONTROL_H__
--- inc/sensors.h
+++ inc/sensors.h
@@ -1,8 +1,20 @@
 #ifndef __SENSORS_H__
 #define __SENSORS_H__
 
-#define ADC_CHANNELS 3
-#define ADC_BUFFER_SIZE 128 * ADC_CHANNELS
+
+// Number of ADC channels for the temperature sensors
+#define T_ADC_CHANNELS 2
+// Number of ADC channels for the humidity sensors
+//@ Mudar para adicionar o senosr de umidade + setar pino PA4
+#define H_ADC_CHANNELS 0
+#define ADC_BUFFER_SIZE (128 * (T_ADC_CHANNELS + H_ADC_CHANNELS))
+
+/**
+ * Public Variables
+ */
+extern uint32_t temperature[T_ADC_CHANNELS];
+//@ Mudar para adicionar o senosr de umidade + setar pino PA4
+// extern uint32_t humidity[H_ADC_CHANNELS];
 
 void sensors_init();
 

0e11c43200808aa1a7f7da952832e9da4463ef91
LucasHaug <LucasHaug@localhost>
2018-10-24 15:52:03 -0300

    Add gnu-debugger, update pi_action logic and add led function

--- src/leds.c
+++ src/leds.c
@@ -1,8 +1,21 @@
 #include "leds.h"
 #include "gpio.h"
 
-//@ Mudar para psicar o led da placa e os leds da camara
 void leds_state(leds_state_t state) {
+    // switch (state) {
+    //     case ON:
+    //         HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, SET);
+    //         break;
+    //     case OFF:
+    //         HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, RESET);
+    //         break;
+    //     case TOGGLE:
+    //         HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+    //         break;
+    // }
+}
+
+void control_led_state(leds_state_t state) {
     switch (state) {
         case ON:
             HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, SET);

138022d01b90e0cc74bb5a6169d20eb93d3d4156
LucasHaug <lucas.haug@hotmail.com>
2018-11-20 14:02:27 -0200

    Add comments and update defines

--- Nucleo/src/control.c
+++ Nucleo/src/control.c
@@ -27,9 +27,20 @@
 #include "sensors.h"
 #include "serial_rx.h"
 
-#define SETPOINT_TEMPERATURE 33
-#define NMAX 10
+/**
+ * @brief Set point temperature in degrees Celsius.
+ */
+#define SETPOINT_TEMPERATURE_C 33
+
+/**
+ * @brief Serial comunication sending period.
+ */
 #define SENDING_PERIOD_MS 1000
+
+/**
+ * @bief Minumum error value for the power resisitors to turn on.
+ */
+#define RESIST_ON_MIN_ERROR 0.5
 
 static float action = 0;
 static float last_action = 0;
@@ -65,7 +76,7 @@ void pi_action(void) {
 #endif  // F3_SERIE
 
         current_time = HAL_GetTick() / 1000;
-        error = SETPOINT_TEMPERATURE - current_int_temperature;
+        error = SETPOINT_TEMPERATURE_C - current_int_temperature;
         action = last_action + kp * (error - last_error) + ki * (current_time - last_time) * error;
         last_time = current_time;
 
@@ -80,7 +91,7 @@ void pi_action(void) {
             set_fan(TOP_FAN, -action);
             set_fan(BOTTOM_FAN, 0);
             resistors_state(OFF);
-        } else if (action > 0 && error > 1) {
+        } else if (action > 0 && error > RESIST_ON_MIN_ERROR) {
             resistors_state(ON);
             set_fan(TOP_FAN, 0);
             // set_fan(BOTTOM_FAN, action * 2 / 3);
@@ -99,7 +110,7 @@ void pi_action(void) {
          */
         if (HAL_GetTick() - time_aux > SENDING_PERIOD_MS) {
             uint8_t fan_on = action < 0;
-            uint8_t heat_on = action > 0 && error > 1;
+            uint8_t heat_on = action > 0 && error > RESIST_ON_MIN_ERROR;
 
             time_aux = HAL_GetTick();
             sprintf(send_data, "s,%.2f,%.2f,%d,%u,%u,e\r\n", current_int_temperature, current_ext_temperature, humidity,
